a19f2eeca132487ac6817c8ab83e33e53740a9f8
timurhan kocabas <timurhankocabas@outlook.com>
2023-05-29 16:52:09 +0200

    adds: routing and bootstrap container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import {Routes,Route} from "react-router-dom"
+import { Container } from "react-bootstrap";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import { About } from "./pages/About";
 
 function App() {
   return (
-    <div>
-      <p>hi</p>
-    </div>
+    <Container>
+      <Routes>
+        <Route path= "/" element={<Home/>}></Route>
+        <Route path= "/store" element={<Store/>}></Route>
+        <Route path= "/about" element={<About/>}></Route>
+      </Routes>
+    </Container>
   );
 }
 
